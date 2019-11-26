8fe297cfb6695f6687208f658f9601d62d79acc9
mnsmanasa <saimanasa1502@gmail.com>
2019-11-26 19:52:02 +0530

    1 file modified

--- SignUp.js
+++ SignUp.js
@@ -1,8 +1,8 @@
 import React,{Component} from 'react'
 
 export default class SignUp extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       loggedIn: false,
       username: '',
@@ -10,30 +10,41 @@ export default class SignUp extends Component {
     }
   }
 
+  componentDidMount() {
+    this.setState(
+      {firstTimeLoad: true}
+    )
+  }
+
   handleInputChange = (event) => {
-    console.log(event.target.name);
     let key = event.target.name
-    key === 'username'? this.setState({username: event.target.value}):this.setState({password: event.target.value})
+    key === 'username'? this.setState({username: event.target.value, usernameError: true}):this.setState({password: event.target.value, passwordError: true})
+  }
+
+  validateForm = () => {
+    if(this.state.username.length === 0 && this.state.password.length === 0){
+      this.setState({usernameError: true,passwordError: true})
+    }
   }
   render(){
     return (
       <div className="signup-form d-flex justify-content-center">
-        <form className="signup-container">
+        <div className="signup-container">
           <h3>Login</h3>
           <div className="signup-field">
             <label>Username</label>
-            <input type="text" name="username" required value={this.state.username} onChange={this.handleInputChange}/>
-            <div className="text-danger">Username is required</div>
+            <input type="text" name="username" required value={ this.state.username} onChange={this.handleInputChange}/>
+            { this.state.usernameError && this.state.username.length === 0 && <div className="text-danger">Username is required</div>}
           </div>
           <div className="signup-field">
             <label>Password</label>
             <input type="password" required name="password" value={this.state.password} onChange={this.handleInputChange}/>
-            <div className="text-danger">Password is required</div>
+            { this.state.passwordError && this.state.password.length === 0 && <div className="text-danger">Password is required</div>}
           </div>
           <div className="signup-field d-flex justify-content-center">
-            <button class="btn btn-outline-primary w-100">Login</button>
+            <button className="btn btn-outline-primary w-100" onClick={this.validateForm}>Login</button>
           </div>
-        </form>
+        </div>
       </div>
     )
   }
