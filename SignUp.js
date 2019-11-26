81b8b2fd6a6a27161be7aae87213f8b2cd1fddd9
mnsmanasa <saimanasa1502@gmail.com>
2019-11-26 18:53:55 +0530

    2 files modified

--- SignUp.js
+++ SignUp.js
@@ -4,14 +4,25 @@ export default class SignUp extends Component {
   constructor(){
     super();
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      username: ''
     }
   }
 
   render(){
     return (
-      <div className="signup-form">
-          
+      <div className="signup-form d-flex justify-content-center">
+        <div className="signup-container">
+          <h3>Login</h3>
+          <div class="signup-field">
+            <label>Username</label>
+            <input type="text" name="username" value={this.state.username} placeholder="please enter username"/>
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" name="username" value={this.state.username} placeholder="please enter username"/>
+          </div>
+        </div>
       </div>
     )
   }
