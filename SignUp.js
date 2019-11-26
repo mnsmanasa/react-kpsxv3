39ec07ae0d0cdd80355b213fbf8045d6cddda65e
mnsmanasa <saimanasa1502@gmail.com>
2019-11-26 19:08:15 +0530

    2 files modified

--- SignUp.js
+++ SignUp.js
@@ -5,24 +5,33 @@ export default class SignUp extends Component {
     super();
     this.state = {
       loggedIn: false,
-      username: ''
+      username: '',
+      password: ''
     }
   }
 
+  handleInputChange = (event) => {
+    this.setState({username: event.target.value})
+  }
   render(){
     return (
       <div className="signup-form d-flex justify-content-center">
-        <div className="signup-container">
+        <form className="signup-container">
           <h3>Login</h3>
-          <div class="signup-field">
+          <div className="signup-field">
             <label>Username</label>
-            <input type="text" name="username" value={this.state.username} placeholder="please enter username"/>
+            <input type="text" name="username" required value={this.state.username} onChange={this.handleInputChange}/>
+            <div className="text-danger">Username is required</div>
           </div>
-          <div>
+          <div className="signup-field">
             <label>Password</label>
-            <input type="password" name="username" value={this.state.username} placeholder="please enter username"/>
+            <input type="password" required name="username" value={this.state.password}/>
+            <div className="text-danger">Password is required</div>
           </div>
-        </div>
+          <div className="signup-field d-flex justify-content-center">
+            <button class="btn btn-outline-primary w-100">Login</button>
+          </div>
+        </form>
       </div>
     )
   }
